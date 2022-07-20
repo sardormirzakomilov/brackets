141100c7c155ef3ac6b443f7a17682be0017bd7b
sardormirzakomilov <mirzakomilovs@gmail.com>
2022-07-20 15:43:37 +0500

    feat: js brockets end

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	
+	let config = [].concat.apply([], bracketsConfig);
+	let openBrackets = config.filter((item, i) => i % 2 === 0);
+	let closeBrackets = config.filter((item, i) => i % 2 === 1);
+	let arr = str.split('');
+	let cache = []; // if the symbols in subarray are equal
+	let stack = []; 
+
+	for(let i = 0; i < arr.length; i++){
+	    outer:
+		for(let j = 0; j < openBrackets.length; j++){
+		    if (openBrackets[j] === closeBrackets[j]){
+		        if(arr[i] === cache[cache.length - 1]){
+                      stack.pop();
+                      cache.pop();
+                  } else {
+                    stack.push(arr[i]);
+                    cache.push(arr[i]);  
+                  }	
+		        break outer;
+		    } else if (arr[i] === openBrackets[j]) {
+			    stack.push(arr[i]);
+			    break outer;
+			} else if (arr[i] === closeBrackets[j]) {
+			    let index = config.indexOf(closeBrackets[j]);
+				let tmp = config[index - 1];
+
+				if (tmp !== stack.pop()) return false;
+				break outer;
+			}   
+		}
+	}
+	
+    return (stack.length === 0) ? true : false;
 }
